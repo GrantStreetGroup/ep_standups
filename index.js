e6b27e3d8f7ae5b4033e77d0f2e02703ab5aa075
Steven Arnott <steven.arnott@grantstreet.com>
2021-10-14 13:54:14 -0400

    Fix spelling

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ exports.expressCreateServer = (hook_name, args, cb)  => {
       padName = createStandupPadName(group);
       logger.info('Pad name '+ padName)
     
-      const exists = awaitpadManager.doesPadExists(padName);
+      const exists = await padManager.doesPadExists(padName);
 
       if (!exists) {
         api.copyPad(defPad,padName,"false")
